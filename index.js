7bbcfcd5163339bf49e836bb86d129c86b4551ea
thejunghare <prasadjunghare2@gmail.com>
2024-03-14 19:37:21 +0530

    - FS module

--- index.js
+++ index.js
@@ -30,3 +30,27 @@ const path = require('path')
 console.log(path.sep);
 console.log(path.join('/parent', 'child', 'file.txt'));
 console.log(path.basename(path.join('/parent', 'child', 'file.txt')));
+
+// FS module 'sync'
+const { readFileSync, writeFileSync, read } = require('fs')
+
+console.log(readFileSync('./parent/file.txt', 'utf8')) // read file
+console.log(readFileSync('./parent/child/file.txt', 'utf8'))
+writeFileSync('./parent/generatedFile.txt', 'Hey!') // create file
+
+// FS module 'Async'
+const { readFile, writeFile } = require('fs')
+
+readFile('./parent/file.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+})
+
+writeFile('./parent/file-async.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+})
