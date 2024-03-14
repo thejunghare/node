2c3f2446c88ea4c44872aab15b952066bc489e5d
thejunghare <prasadjunghare2@gmail.com>
2024-03-14 14:06:19 +0530

    - add alternate way to export module

--- fnames.js
+++ fnames.js
@@ -2,4 +2,6 @@ const rock = 'rock'
 const john = 'john'
 const roman = 'roman'
 
-module.exports = {rock, john, roman}
+module.exports = { rock, john, roman }
+
+module.exports.names = ['seth', 'brock']
